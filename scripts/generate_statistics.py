5f00cfbbc05ee564a18ba92a2282bb980dfdc49d
woseseltops <stoopwessel@gmail.com>
2016-12-14 14:37:20 +0100

    Added automatically colored chairs

--- scripts/generate_statistics.py
+++ scripts/generate_statistics.py
@@ -2,7 +2,7 @@ def main(fake=False):
     """Should return a dictionary of the shape {'party1':140,'party2': 10}"""
 
     if fake:
-        return {'vvd':40,'pvda':36,'sp':15,'cda':13,'d66':12,'pvv':12,'gl':5,'pvvd':5,'cu':4,'50+':4,'sgp':4}
+        return {'vvd':40,'pvda':36,'sp':15,'cda':13,'d66':12,'pvv':12,'gl':5,'pvdd':5,'cu':4,'vijftigplus':4,'sgp':4}
     else:
 
         return None
