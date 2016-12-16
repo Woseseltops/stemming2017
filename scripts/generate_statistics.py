aed2f221a76826d3ee1103dc5a331ffe4ab41500
Wessel Stoop <stoopwessel@gmail.com>
2016-12-16 12:13:20 +0100

    Added graph and image to template

--- scripts/generate_statistics.py
+++ scripts/generate_statistics.py
@@ -1,8 +1,23 @@
-def main(fake=False):
+def get_chairs_per_party(fake=False):
     """Should return a dictionary of the shape {'party1':140,'party2': 10}"""
 
     if fake:
         return {'vvd':40,'pvda':36,'sp':15,'cda':13,'d66':12,'pvv':12,'gl':5,'pvdd':5,'cu':4,'vijftigplus':4,'sgp':4}
+    else:
+
+        return None
+
+def get_history_of_party_mentions(fake=False):
+    """Should return a list of dictionaries of the shape [{'party1':14000,'party2': 1000},{'party1':15000,'party2': 800}]"""
+
+    if fake:
+        return [{'vvd': 1000,'pvda': 600,'sp': 300,'cda': 200,'d66': 120,'pvv': 350,'gl': 50,'pvdd': 20,'cu': 10,'vijftigplus': 20,
+                 'sgp': 40},
+                {'vvd': 1100,'pvda':  1600,'sp': 200,'cda': 220,'d66': 800,'pvv': 30,'gl': 150,'pvdd': 20,'cu':  10,
+                 'vijftigplus': 20,'sgp': 40},
+                {'vvd': 800, 'pvda': 1200, 'sp': 300, 'cda': 270, 'd66': 300, 'pvv': 1000, 'gl': 230, 'pvdd': 100,
+                 'cu': 100, 'vijftigplus': 100, 'sgp': 350},
+                ]*2
     else:
 
         return None
--- scripts/refresh_static_page.py
+++ scripts/refresh_static_page.py
@@ -11,9 +11,9 @@ def backup_previous_static_page(static_page_location,backup_folder):
     except FileNotFoundError:
         pass
 
-def refresh_static_page(chairs_per_party,template):
+def refresh_static_page(chairs_per_party,history_of_party_mentions,template):
 
-    #Translate the statistics to a value for each pair
+    #Translate the chair statistics to a value for each chair
     chairs_per_party = sorted(chairs_per_party.items(),key=lambda x: x[1],reverse=True)
     chairs = []
 
@@ -21,7 +21,27 @@ def refresh_static_page(chairs_per_party,template):
         for chair in range(value):
             chairs.append(party)
 
-    return Template(open(template).read()).render(chairs=enumerate(chairs),chairs_per_party=chairs_per_party)
+    #Translate the history of party mentions to series of percentages
+    series_of_percentages_per_party = {}
+
+    for mentions_in_period in history_of_party_mentions:
+        total_mentions_this_period = sum(mentions_in_period.values())
+
+        for party_name, number_of_mentions in mentions_in_period.items():
+
+            percentage = 100*(number_of_mentions/total_mentions_this_period)
+
+            try:
+                series_of_percentages_per_party[party_name].append(percentage)
+            except KeyError:
+                series_of_percentages_per_party[party_name] = [percentage]
+
+    #Order the series, so the parties will always be presented to the Javascript in the same order
+    series_of_percentages_per_party = sorted(series_of_percentages_per_party.items(),key=lambda x: x[0])
+
+    #Generate the page
+    return Template(open(template).read()).render(chairs=enumerate(chairs),chairs_per_party=chairs_per_party,
+                                                  series_of_percentages_per_party=series_of_percentages_per_party)
 
 if __name__ == '__main__':
 
@@ -29,7 +49,8 @@ if __name__ == '__main__':
     backup_previous_static_page(current_settings['static_page'],current_settings['previous_static_pages_folder'])
 
     #Create a new one
-    chairs_per_party = generate_statistics.main(fake=True)
-    new_page_content = refresh_static_page(chairs_per_party,current_settings['template'])
+    chairs_per_party = generate_statistics.get_chairs_per_party(fake=True)
+    history_of_party_mentions = generate_statistics.get_history_of_party_mentions(fake=True)
+    new_page_content = refresh_static_page(chairs_per_party,history_of_party_mentions,current_settings['template'])
 
     open(current_settings['static_page'],'w').write(new_page_content)
